d2a0aa5edcfe292fee5c8a8e6fa0d286488a99e3
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-06-22 02:39:45 +0200

    fix: text repeating over and over

--- new.py
+++ new.py
@@ -16,22 +16,33 @@ rows = []
 dataframe = openpyxl.load_workbook(data)
 dataframe1 = dataframe.active
 for i in dataframe1.iter_rows(
-        min_row=int(begin)+2, min_col=0, max_row=int(to)+2, max_col=71, values_only=True
+    min_row=int(begin) + 2, min_col=0, max_row=int(to) + 2, max_col=71, values_only=True
 ):
     rows.append(i)
 
 driver = webdriver.Chrome()
+driver.delete_all_cookies()
 driver.get("http://sensus-kkp.argocipta.com/login")
 # driver.find_element(By.NAME,"username").send_keys(os.environ.get("username"))
 # driver.find_element(By.ID,"password-field").send_keys(os.environ.get("password"))
 # driver.find_element(By.CLASS_NAME,"btn").click()
 easygui.msgbox("Login dulu, terus klik OK")
 
+
+def slow_type(element, text, delay=0.0005):
+    element.send_keys(" ")
+    element.clear()
+    for character in text:
+        element.send_keys(character)
+        time.sleep(delay)
+
+
 def get_sumber_modal(name):
-    name =  name.lower()
-    if name== "sendiri" or name == "mandiri":
-        return  "sendiri"
+    name = name.lower()
+    if name == "sendiri" or name == "mandiri":
+        return "sendiri"
     return "kredit"
+
 
 def get_tanggal_lahir(NIK, gender):
     val = NIK[6:12]
@@ -49,6 +60,7 @@ def get_tanggal_lahir(NIK, gender):
         year += 2000
 
     return str(year) + "-" + str(month) + "-" + str(date)
+
 
 def get_pendidikan(name):
     name = name.lower()
@@ -74,15 +86,18 @@ def get_pendidikan(name):
         return 11
     return 99
 
+
 def select_from_option(value, possible_values):
+
     for i in range(len(possible_values)):
         if value == possible_values[i]:
             return i
     return len(possible_values) - 1
 
+
 for i in range(len(rows)):
     num = rows[i][0]
-    
+
     kelompok = rows[i][16]
     biota = rows[i][17]
     komoditas = rows[i][15]
@@ -153,6 +168,7 @@ for i in range(len(rows)):
     #     biota,
     #     komoditas,
     #     NIK,
+    #     nama,
     #     tanggal_lahir,
     #     pendidikan,
     #     anggota_keluarga,
@@ -204,75 +220,126 @@ for i in range(len(rows)):
     Select(driver.find_element(By.NAME, "kelompok_id")).select_by_visible_text(kelompok)
     Select(driver.find_element(By.NAME, "biota_id")).select_by_visible_text(biota)
     time.sleep(2)
-    Select(driver.find_element(By.NAME, "ikan_id")).select_by_visible_text(komoditas.title())
+    Select(driver.find_element(By.NAME, "ikan_id")).select_by_visible_text(
+        komoditas.title()
+    )
 
-    driver.find_element(By.NAME, "nik").send_keys(NIK)
-    Select(driver.find_element(By.NAME, "agama_id")).select_by_visible_text(agama.title())
-    driver.find_element(By.NAME, "name").send_keys(nama)
-    driver.find_element(By.NAME, "birthdate").send_keys(tanggal_lahir)
-    Select(driver.find_element(By.NAME, "pendidikan")).select_by_value(str(get_pendidikan(pendidikan)))
-    driver.find_element(By.NAME, "family_num").send_keys(anggota_keluarga)
-    
-    driver.find_element(By.NAME, "address").send_keys(alamat)
-    Select(driver.find_element(By.NAME, "kecamatan_id")).select_by_visible_text(kecamatan)
+    # cahnge all send_keys to slow_type()
+
+    slow_type(driver.find_element(By.NAME, "nik"), NIK)
+    time.sleep(1)
+    Select(driver.find_element(By.NAME, "agama_id")).select_by_visible_text(
+        agama.title()
+    )
+    slow_type(driver.find_element(By.NAME, "name"), nama, delay=0.03)
+    slow_type(driver.find_element(By.NAME, "birthdate"), tanggal_lahir)
+    Select(driver.find_element(By.NAME, "pendidikan")).select_by_value(
+        str(get_pendidikan(pendidikan))
+    )
+    slow_type(driver.find_element(By.NAME, "family_num"), str(anggota_keluarga))
+
+    slow_type(driver.find_element(By.NAME, "address"), alamat, delay=0.03)
+    Select(driver.find_element(By.NAME, "kecamatan_id")).select_by_visible_text(
+        kecamatan
+    )
 
     Select(driver.find_element(By.NAME, "kelurahan_id")).select_by_visible_text(desa)
-    driver.find_element(By.NAME, "lat").send_keys(latitude)
-    driver.find_element(By.NAME, "lng").send_keys(longitude)
 
-    Select(driver.find_element(By.NAME, "jenis_usaha")).select_by_visible_text(jenis_usaha.title())
-    Select(driver.find_element(By.NAME, "status_kusuka")).select_by_visible_text(status_kusuka)
-    Select(driver.find_element(By.NAME, "status_milik")).select_by_visible_text(kepemilikan)
-    driver.find_element(By.NAME, "luas_usaha").send_keys(luas)
-    driver.find_element(By.NAME, "media_pelihara").send_keys(media)
+    slow_type(driver.find_element(By.NAME, "lat"), str(latitude), delay=0.03)
+
+    slow_type(driver.find_element(By.NAME, "lng"), str(longitude), delay=0.03)
+
+    Select(driver.find_element(By.NAME, "jenis_usaha")).select_by_visible_text(
+        jenis_usaha.title()
+    )
+    Select(driver.find_element(By.NAME, "status_kusuka")).select_by_visible_text(
+        status_kusuka
+    )
+    Select(driver.find_element(By.NAME, "status_milik")).select_by_visible_text(
+        kepemilikan
+    )
+
+    slow_type(driver.find_element(By.NAME, "luas_usaha"), str(luas))
+
+    Select(driver.find_element(By.NAME, "media_pelihara")).select_by_visible_text(
+        media.title()
+    )
+
     Select(driver.find_element(By.NAME, "teknologi")).select_by_visible_text(tekonologi)
 
+    slow_type(driver.find_element(By.NAME, "produksi"), str(produktifitas))
+    slow_type(driver.find_element(By.NAME, "harga"), str(harga_jual))
+    slow_type(driver.find_element(By.NAME, "income"), str(pendapatan))
 
-    driver.find_element(By.NAME, "produksi").send_keys(produktifitas)
-    driver.find_element(By.NAME, "harga").send_keys(harga_jual)
-    driver.find_element(By.NAME, "income").send_keys(pendapatan)
+    Select(driver.find_element(By.NAME, "pakan_jenis")).select_by_visible_text(
+        jenis_pakan.title()
+    )
+    slow_type(driver.find_element(By.NAME, "pakan_num"), str(jumlah_pakan))
+    slow_type(driver.find_element(By.NAME, "pakan_harga"), str(harga_pakan))
+    slow_type(driver.find_element(By.NAME, "biaya_pakan"), str(harga_pembelian_pakan))
 
-    Select(driver.find_element(By.NAME, "pakan_jenis")).select_by_visible_text(jenis_pakan.title())
-    driver.find_element(By.NAME, "pakan_num").send_keys(jumlah_pakan)
-    driver.find_element(By.NAME, "pakan_harga").send_keys(harga_pakan)
-    driver.find_element(By.NAME, "biaya_pakan").send_keys(harga_pembelian_pakan)
+    slow_type(driver.find_element(By.NAME, "benur_num"), str(jumlah_benih))
+    slow_type(driver.find_element(By.NAME, "benur_harga"), str(harga_benih))
+    slow_type(driver.find_element(By.NAME, "biaya_benih"), str(harga_pembelian))
 
-    driver.find_element(By.NAME, "benur_num").send_keys(jumlah_benih)
-    driver.find_element(By.NAME, "benur_harga").send_keys(harga_benih)
-    driver.find_element(By.NAME, "biaya_benih").send_keys(harga_pembelian)
-    
-    driver.find_element(By.NAME, "tk_num").send_keys(jumlah_tk)
-    driver.find_element(By.NAME, "omzet").send_keys(besaran_modal)
+    slow_type(driver.find_element(By.NAME, "tk_num"), str(jumlah_tk))
+    slow_type(driver.find_element(By.NAME, "omzet"), str(besaran_modal))
 
-    Select(driver.find_element(By.NAME, "sumber_modal")).select_by_visible_text(sumber_modal.title())
-    driver.find_element(By.NAME, "biaya_media").send_keys(biaya_pembuatan_media)
-    driver.find_element(By.NAME, "biaya_susut").send_keys(biaya_penyusutan)
-    driver.find_element(By.NAME, "biaya_alat").send_keys(biaya_peralatan)
-    driver.find_element(By.NAME, "biaya_tk").send_keys(biaya_tenaga_kerja)
+    Select(driver.find_element(By.NAME, "sumber_modal")).select_by_visible_text(
+        sumber_modal.title()
+    )
+    slow_type(driver.find_element(By.NAME, "biaya_media"), str(biaya_pembuatan_media))
+    slow_type(driver.find_element(By.NAME, "biaya_susut"), str(biaya_penyusutan))
+    slow_type(driver.find_element(By.NAME, "biaya_alat"), str(biaya_peralatan))
+    slow_type(driver.find_element(By.NAME, "biaya_tk"), str(biaya_tenaga_kerja))
 
     Select(driver.find_element(By.NAME, "ipal")).select_by_visible_text(IPAL.title())
-    Select(driver.find_element(By.NAME, "tandon")).select_by_visible_text(tandon.title())
-    Select(driver.find_element(By.NAME, "greenbelt")).select_by_visible_text(green_belt.title())
-    driver.find_element(By.NAME, "jarak_tambak").send_keys(jarak_ke_pantai)
-    driver.find_element(By.NAME, "sumber_air").send_keys(sumber_air)
+    Select(driver.find_element(By.NAME, "tandon")).select_by_visible_text(
+        tandon.title()
+    )
+    Select(driver.find_element(By.NAME, "greenbelt")).select_by_visible_text(
+        green_belt.title()
+    )
+    slow_type(driver.find_element(By.NAME, "jarak_tambak"), str(jarak_ke_pantai))
+    slow_type(driver.find_element(By.NAME, "sumber_air"), sumber_air)
 
-    Select(driver.find_element(By.NAME, "izin")).select_by_visible_text(perizinan.title())
-    Select(driver.find_element(By.NAME, "nib")).select_by_visible_text(status_NIB.title())
-    Select(driver.find_element(By.NAME, "skala_usaha")).select_by_visible_text(skala_usaha.title())
+    Select(driver.find_element(By.NAME, "izin")).select_by_visible_text(
+        perizinan.title()
+    )
+    Select(driver.find_element(By.NAME, "nib")).select_by_visible_text(
+        status_NIB.title()
+    )
+    Select(driver.find_element(By.NAME, "skala_usaha")).select_by_visible_text(
+        skala_usaha.title()
+    )
 
-    Select(driver.find_element(By.NAME, "asuransi")).select_by_visible_text(asuransi.title())
-    Select(driver.find_element(By.NAME, "bantuan")).select_by_visible_text(bantuan.title())
-    Select(driver.find_element(By.NAME, "penghargaan")).select_by_visible_text(penghargaan.title())
-    Select(driver.find_element(By.NAME, "dukungan_pemda")).select_by_visible_text(dukungan_pemda.title())
-    Select(driver.find_element(By.NAME, "dukungan_pusat")).select_by_visible_text(dukungan_pusat.title())
-    Select(driver.find_element(By.NAME, "sertifikat")).select_by_visible_text(sertifikat.title())
+    Select(driver.find_element(By.NAME, "asuransi")).select_by_visible_text(
+        asuransi.title()
+    )
+    Select(driver.find_element(By.NAME, "bantuan")).select_by_visible_text(
+        bantuan.title()
+    )
+    Select(driver.find_element(By.NAME, "penghargaan")).select_by_visible_text(
+        penghargaan.title()
+    )
+    Select(driver.find_element(By.NAME, "dukungan_pemda")).select_by_visible_text(
+        dukungan_pemda.title()
+    )
+    Select(driver.find_element(By.NAME, "dukungan_pusat")).select_by_visible_text(
+        dukungan_pusat.title()
+    )
+    Select(driver.find_element(By.NAME, "sertifikat")).select_by_visible_text(
+        sertifikat.title()
+    )
 
-    driver.find_element(By.NAME, "penyuluh_name").send_keys(nama_penyuluh)
+    slow_type(driver.find_element(By.NAME, "penyuluh_name"), nama_penyuluh)
 
-    easygui.msgbox("Check dulu datanya lengkap atau tidak. Kalau sudah yakin, klik OK untuk lanjut")
+    easygui.msgbox(
+        "Check dulu datanya lengkap atau tidak. Kalau sudah yakin, klik OK untuk lanjut"
+    )
     driver.find_element(By.ID, "submitUser").click()
 
-    if  not easygui.ynbox("Lanjut?"):
+    if not easygui.ynbox("Lanjut?"):
         break
 
 
